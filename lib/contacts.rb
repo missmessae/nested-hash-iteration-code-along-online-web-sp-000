42ac98c917008b8907c2e7d5044461b27e9758a0
Annie Wiegel <annewiegel17@gmail.com>
2021-02-08 00:03:04 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,6 +15,6 @@ require 'pry'
   # }
 
 def remove_strawberry(contacts)
-  contacts["Freddy Mercury"][:favorite_ice_cream_flavors].delete_at(0)
+  contacts["Freddy Mercury"][:favorite_ice_cream_flavors].delete("strawberry")
   contacts
 end
